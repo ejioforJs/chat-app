27bd5b1f70a0a80b5c638ba3aceda702b737b623
solomon james <ejioforjames12@gmail.com>
2023-03-19 20:34:36 -0700

    chat-app

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,7 @@ const server = app.listen(PORT, console.log(`server started on port ${PORT}`));
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chat-app-genu.onrender.com',
   },
 });
 
